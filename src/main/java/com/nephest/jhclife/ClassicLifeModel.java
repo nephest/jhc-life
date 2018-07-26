9dc2a9b48f84f5422032c1bec9a7694bdc1ed7b7
Oleksandr Masniuk <nephestdev@gmail.com>
2018-07-26 14:10:33 +0300

    make ClassicLifeModel thread safe

--- src/main/java/com/nephest/jhclife/ClassicLifeModel.java
+++ src/main/java/com/nephest/jhclife/ClassicLifeModel.java
@@ -23,17 +23,25 @@
 package com.nephest.jhclife;
 
 import java.util.*;
+import java.util.logging.*;
 import java.util.concurrent.*;
 
 public class ClassicLifeModel
+implements java.io.Closeable
 {
+
+    private static final Logger LOG
+        = Logger.getLogger(ClassicLifeModel.class.getName());
 
     public static final int POPULATION_MIN = 2;
     public static final int POPULATION_MAX = 3;
     public static final int POPULATION_REPRODUCTION = 3;
 
     private final ForkJoinPool forkJoinPool;
+    private final ScheduledExecutorService executor;
     private final Random random = new Random();
+    private GenerationCalculator generationCalculator;
+    private ScheduledFuture generationFuture;
 
     private int width;
     private int height;
@@ -44,20 +52,50 @@ public class ClassicLifeModel
     private long generation = 0;
     private long id = 0;
 
-    public ClassicLifeModel(int width, int height, ForkJoinPool pool)
+    private volatile boolean closed = false;
+    private boolean externalExecutor = true;
+    private volatile boolean running = false;
+    private long generationLifeTimePeriod = 1;
+    private TimeUnit generationLifeTimeUnit = TimeUnit.SECONDS;
+
+    public ClassicLifeModel
+    (
+        int width,
+        int height,
+        ForkJoinPool pool,
+        ScheduledExecutorService executor
+    )
     {
+        Objects.requireNonNull(executor);
         this.width = width;
         this.height = height;
         createNewPopulation(width, height);
         this.forkJoinPool = pool;
+        this.executor = executor;
     }
 
     public ClassicLifeModel(int width, int height)
     {
-        this(width, height, null);
+        this
+        (
+            width,
+            height,
+            null,
+            Executors.newSingleThreadScheduledExecutor
+            (
+                (r)->
+                {
+                    Thread t = Executors.defaultThreadFactory().newThread(r);
+                    t.setDaemon(false);
+                    t.setName("ClassicLifeModel manager");
+                    return t;
+                }
+            )
+        );
+        this.externalExecutor = false;
     }
 
-    public final void createNewPopulation(int width, int height)
+    public final synchronized void createNewPopulation(int width, int height)
     {
         if (width < 0) throw new IllegalArgumentException("width must be more than 0");
         if (height < 0) throw new IllegalArgumentException("height must be more than 0");
@@ -70,14 +108,43 @@ public class ClassicLifeModel
         externalModification();
     }
 
+    @Override
+    public void close()
+    {
+        if (isClosed()) return;
+        stop();
+        if (!isExternalExecutor()) getExecutor().shutdown();
+        this.closed = true;
+    }
+
+    public boolean isClosed()
+    {
+        return this.closed;
+    }
+
     private ForkJoinPool getForkJoinPool()
     {
         return this.forkJoinPool;
     }
 
+    protected ScheduledExecutorService getExecutor()
+    {
+        return this.executor;
+    }
+
     private Random getRandom()
     {
         return this.random;
+    }
+
+    private GenerationCalculator getGenerationCalculator()
+    {
+        return this.generationCalculator;
+    }
+
+    private ScheduledFuture getGenerationFuture()
+    {
+        return this.generationFuture;
     }
 
     public int getWidth()
@@ -125,12 +192,78 @@ public class ClassicLifeModel
         return this.id;
     }
 
+    private boolean isExternalExecutor()
+    {
+        return this.externalExecutor;
+    }
+
+    public boolean isRunning()
+    {
+        return this.running;
+    }
+
+    public synchronized void start()
+    {
+        if (isRunning()) return;
+        if (isClosed())
+            throw new IllegalStateException("Can't start model. Resources are closed");
+        this.generationFuture = getExecutor().scheduleAtFixedRate
+        (
+            ()->nextGeneration(),
+            this.generationLifeTimePeriod,
+            this.generationLifeTimePeriod,
+            this.generationLifeTimeUnit
+        );
+        this.running = true;
+    }
+
+    public synchronized void stop()
+    {
+        if(!isRunning()) return;
+        getGenerationFuture().cancel(false);
+        if(getGenerationCalculator() != null)
+            getGenerationCalculator().cancel(false);
+        boolean got = getGenerationFuture().isCancelled();
+        while (!got)
+        {
+            try
+            {
+                getGenerationFuture().get();
+                got = true;
+            }
+            catch (InterruptedException ex)
+            {
+                LOG.log(Level.SEVERE, ex.getMessage(), ex);
+            }
+            catch (ExecutionException ex)
+            {
+                LOG.log(Level.SEVERE, ex.getMessage(), ex);
+                got = true;
+            }
+        }
+        this.running = false;
+    }
+
+    public long getGenerationLifeTime(TimeUnit unit)
+    {
+        return unit.convert(this.generationLifeTimePeriod, this.generationLifeTimeUnit);
+    }
+
+    public synchronized void setGenerationLifeTime(long period, TimeUnit unit)
+    {
+        boolean wasRunning = isRunning();
+        if (wasRunning) stop();
+        this.generationLifeTimePeriod = period;
+        this.generationLifeTimeUnit = unit;
+        if (wasRunning) start();
+    }
+
     private void externalModification()
     {
         this.id++;
     }
 
-    public void populate(long seed, double populationProbability)
+    public synchronized void populate(long seed, double populationProbability)
     {
         if (populationProbability < 0 || populationProbability > 1.0)
             throw new IllegalArgumentException("population probability must be in 0-1 range");
@@ -173,20 +306,20 @@ public class ClassicLifeModel
         return getRandom().nextDouble() < getPopulationProbability();
     }
 
-    public void nextGeneration()
+    protected void nextGeneration()
     {
-        GenerationCalculator calc
+        this.generationCalculator
             = new GenerationCalculator(getLastPopulation(), getPopulation());
         ForkJoinPool pool
             = getForkJoinPool() == null
             ? ForkJoinPool.commonPool()
             : getForkJoinPool();
-        pool.invoke(calc);
+        pool.invoke(getGenerationCalculator());
         savePopulation();
         this.generation++;
     }
 
-    public void setPopulation(int x, int y, boolean pop)
+    public synchronized void setPopulation(int x, int y, boolean pop)
     {
         if (x < 0 || x > getWidth())
             throw new IllegalArgumentException("x out of bounds");
