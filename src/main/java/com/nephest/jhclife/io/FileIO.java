075474a98ef7c7f8a278573e2b376648c8ea33ac
Oleksandr Masniuk <nephestdev@gmail.com>
2018-08-21 12:14:24 +0300

    init fileio

---
+++ src/main/java/com/nephest/jhclife/io/FileIO.java
@@ -0,0 +1,40 @@
+/*-
+ * =========================LICENSE_START=========================
+ * jhc-life
+ * %%
+ * Copyright (C) 2018 Oleksandr Masniuk
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * =========================LICENSE_END=========================
+ */
+
+package com.nephest.jhclife.io;
+
+import java.io.*;
+import java.nio.file.*;
+
+public interface FileIO
+{
+
+    public OutputStream getOutputStream(File file)
+    throws FileNotFoundException;
+
+    public void write(Path path, byte[] bytes, OpenOption... options)
+    throws IOException;
+
+    public byte[] readAllBytes(Path path)
+    throws IOException;
+
+}
